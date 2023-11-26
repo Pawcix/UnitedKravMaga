7c0b129398a1bbd94c51f7952189aeeec6f40e24
pawcix <Pawcix@op.pl>
2023-11-26 23:47:45 +0100

    finish first template

--- src/js/bgc.js
+++ src/js/bgc.js
@@ -1,1 +1,11 @@
-// NAPISAĆ FUNKCJE POKAZUWANIA SIĘ TŁA NAV NA MOBILE
+const nav = document.querySelector('.navbar');
+
+const tlo = () => {
+    if (window.scrollY > 106 && window.screen.width < 991) {
+        nav.classList.add("bgc");
+    } else {
+        nav.classList.remove("bgc");
+    }
+};
+
+window.addEventListener('scroll', tlo);
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,9 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const mobileMenuToggle = document.getElementById('mobile-menu');
-    const navList = document.getElementById('nav-list');
+const nav = document.querySelector('.navbar');
+const mobileMenu = document.getElementById('mobile-menu');
+const navList = document.getElementById('nav-list');
 
-    mobileMenuToggle.addEventListener('click', function () {
-        mobileMenuToggle.querySelector('.burger-icon').classList.toggle('active');
-        navList.classList.toggle('show');
-    });
-});
+const mobileMenuClick = () => {
+    mobileMenu.querySelector('.burger-icon').classList.toggle('active');
+    navList.classList.toggle('show');
+
+    if (navList.classList.contains('show')) {
+        nav.classList.remove('bgc');
+    }
+}
+
+mobileMenu.addEventListener('click', mobileMenuClick);
